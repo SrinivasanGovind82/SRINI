9ab6d58055e45d018cfa18ef1d15264b9a687887
SrinivasanGovind82 <srinivasan635107@gmail.com>
2021-02-05 19:09:04 +0530

    DONE KAARO

---
+++ src/org/all/LanguageClass.java
@@ -0,0 +1,23 @@
+package org.all;
+
+import org.telgu.Telgu;
+
+public class LanguageClass extends Telgu {
+public void allLanguage() {
+System.out.println("all over world");
+}
+public static void main(String[] args) {
+LanguageClass l= new LanguageClass();
+l.allLanguage();
+l.telguLanguage();
+l.englishLanguage();
+l.tamilLanguage();
+
+	
+}
+
+
+
+
+
+}
